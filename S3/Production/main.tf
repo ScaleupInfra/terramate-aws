ba96d6c70db3e2a48ccbed5af44698791f431bd1
SIDVJSINGH <sidvjsingh@gmail.com>
2023-08-24 16:29:05 +0530

    tags changed

--- S3/Production/main.tf
+++ S3/Production/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "Infrasity" {
   acl = "private"
   tags = {
     name    = "S3"
-    made_by = "Infrasity"
+    made_by = "Scaleup-Infra"
   }
   versioning {
     enabled = true
