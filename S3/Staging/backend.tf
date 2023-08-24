1b65c5e591a4729f587c9f0a0257fd01e23406f2
SIDVJSINGH <sidvjsingh@gmail.com>
2023-08-24 13:01:04 +0530

    empty backend

--- S3/Staging/backend.tf
+++ S3/Staging/backend.tf
@@ -1,10 +1,2 @@
 // TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
 
-terraform {
-  backend "s3" {
-    bucket  = "terraform-backend-state-file-s3"
-    encrypt = true
-    key     = "terraform-S3/terraform.tfstate"
-    region  = "ap-northeast-1"
-  }
-}
